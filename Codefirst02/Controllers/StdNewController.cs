3af332ff7332d33d19ae81d8d9d1e70c00a1c4df
TranThiHue999 <tranthihue20899@gmail.com>
2021-09-24 12:46:55 +0700

    Hue09

--- Codefirst02/Controllers/StdNewController.cs
+++ Codefirst02/Controllers/StdNewController.cs
@@ -12,6 +12,7 @@ namespace Codefirst02.Controllers
     {
         //khai bao DB context de lam viec voi Database
         private LapTrinhQuanLyDBContext db = new LapTrinhQuanLyDBContext();
+        AutoGenerateKey genKey = new AutoGenerateKey();
         // GET: StdNew
         public ActionResult Index()
         {
@@ -19,7 +20,37 @@ namespace Codefirst02.Controllers
         }
         public ActionResult Create()
         {
+            var stdID = "";
+            var countStudent = db.Students.Count();
+            if (countStudent == 0)
+            {
+                stdID = "STD001";
+            }
+            else
+            {
+
+                // lay gia tri id moi nhat
+                var studentID = db.Students.ToList().OrderByDescending(m => m.StudentID).FirstOrDefault().StudentID;
+                //sinh studentId tu dong
+                stdID = genKey.GenerateKey(studentID);
+            }
+            ViewBag.StudentID = stdID;
             return View();
         }
+        [HttpPost]
+        public ActionResult Create(Student Std)
+        {
+            if (ModelState.IsValid)
+            { 
+            //luu tt vao db
+            db.Students.Add(Std);
+            db.SaveChanges();
+                //lay du lieu clien gui len va luu vao database
+                return RedirectToAction("Index");
+            }
+            
+            return View(Std);
+        }
+
     }
 }
--- Codefirst02/Models/AutoGenerateKey.cs
+++ Codefirst02/Models/AutoGenerateKey.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Codefirst02.Models
 {
     public class AutoGenerateKey
     {
-        public string GenerateKey( string id )
+        public string GenerateKey ( string id )
         {
-            //kiem tra gia tri id truyen vao la rong hay khong
-            //neu id khong null 
-            //check rieng phan so va phan chu cua tham so id
-            //giu nguyen phan chu
-            //phan so chuyen doi sang kieu so nguyen va tang len 1 don vi
-            //ghep phan so voi phan chu de duoc ma tu sinh
-            // tra ve ma sau khi tu sinh
-            
-            string strkey = "";
-            return strkey;
-        }
+            string strkey ="";
+
+            string numPart ="", strPart = "";
+
+            numPart = Regex.Match(id, @"\d+").Value;
+
+            strkey = strPart + (Convert.ToInt32(numPart) +1);
+
+
     }
 }
