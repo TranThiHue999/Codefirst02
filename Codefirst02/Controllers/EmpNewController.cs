d09e3dd4967fe8870e331c9733ccc0b3630c71d5
TranThiHue999 <tranthihue20899@gmail.com>
2021-09-28 14:12:34 +0700

    hue208999

---
+++ Codefirst02/Controllers/EmpNewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Codefirst02.Controllers
+{
+    public class EmpNewController : Controller
+    {
+        // GET: EmpNew
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Codefirst02/Models/Employee.cs
+++ Codefirst02/Models/Employee.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Codefirst02.Models
 {
     public class Employee: Person
     {
         public String Company { get; set; }
+        [AllowHtml]
         public String Adress { get; set; }
     }
 }
